e118de669ce845ee3819427f6f4dcde3ee45cb3b
Yi Qingliang <niqingliang2003@gmail.com>
2022-10-09 11:07:24 +0800

    build app at 0x0
    Signed-off-by: Yi Qingliang <niqingliang2003@gmail.com>

--- user/src/lib.rs
+++ user/src/lib.rs
@@ -23,15 +23,5 @@ fn main() -> i32 {
     panic!("Cannot find main!");
 }
 
-fn clear_bss() {
-    extern "C" {
-        fn sbss();
-        fn ebss();
-    }
-    (sbss as usize..ebss as usize).for_each(|a| {
-        unsafe { (a as *mut u8).write_volatile(0) }
-    });
-}
-
 pub fn write(fd: usize, buf: &[u8]) -> isize { sys_write(fd, buf) }
 pub fn exit(exit_code: i32) -> isize { sys_exit(exit_code) }
